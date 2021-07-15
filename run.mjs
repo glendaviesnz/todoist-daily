1099e9b5391c64cf6c25966f11eceafeaad0e9d4
Glen Davies <glen.davies@a8c.com>
2021-07-15 18:55:33 +1200

    Add custom emojis

--- run.mjs
+++ run.mjs
@@ -9,7 +9,7 @@ let slackMessage;
 dayjs.extend(isYesterday);
 dayjs.extend(isToday)
 
-slackMessage = "*How do you feel today?*\n\n:green-dot:\n\n*What did you do yesterday?*\n\n";
+slackMessage = "*Glen Davies* posted an update for *Dotcom View Standup*\n\n*How do you feel today?*\n\n:green-dot:\n\n*What did you do yesterday?*\n\n";
 const api = axios.create({
     headers: {'Authorization': `Bearer ${config.todoistToken}`}
   });
@@ -19,22 +19,19 @@ api.get('https://api.todoist.com/sync/v8/completed/get_all').then((result) => {
         return item.project_id === config.todoistProject && dayjs(item.completed_date).isYesterday();
     }).map(item => item.content);
    
-    yesterdaysTasks.forEach((task) => slackMessage += ':keyboard: ' + formatTask(task) + "\n");
+    yesterdaysTasks.forEach((task) => slackMessage += formatTask(task) + "\n");
 
     api.get('https://api.todoist.com/sync/v8/sync?sync_token=*&resource_types=[\"items\"]').then((result) => {
         const todaysTasks = result.data.items.filter(item => {
             return item.project_id === config.todoistProject && dayjs(item.due.date).isToday();
         }).map(item => item.content);
-        slackMessage += "\n\n*What you plan to do today?* :\n\n";
-        todaysTasks.forEach((task) => slackMessage += ':keyboard: ' + formatTask(task) + "\n");
+        slackMessage += "\n\n*What will you do today?*\n\n";
+        todaysTasks.forEach((task) => slackMessage += formatTask(task) + "\n");
+        slackMessage += "\n\n*Anything blocking your progress?*\n\nNo";
     }).then(() => {
         run().catch(err => console.log(err));
-        // console.log(slackMessage);
     });
 });
-
-
-
 
 
 async function run() {
@@ -45,12 +42,27 @@ async function run() {
       unfurl_links: false,
       unfurl_media: false
   }, { headers: { authorization: `Bearer ${config.slackToken}` } });
-
-  console.log('Done', res.data);
 }
 
 function formatTask(task) {
     const markdownLinkRegex = /\[(.*)\]\(((?:\/|https?:\/\/)[\w\d.\/?=#-_]+)\)/g;
+    const linkedTask = task.replace(markdownLinkRegex, "<$2|$1>");
 
-    return task.replace(markdownLinkRegex, "<$2|$1>");
+    if (task.toLowerCase().includes('review')) {
+        return `:eyes: ${linkedTask}`;
+    }
+    if (task.toLowerCase().includes('test')) {
+        return `:crash-test-dummy: ${linkedTask}`;
+    }
+    if (task.toLowerCase().includes('read')) {
+        return `:reading: ${linkedTask}`;
+    }
+    if (task.toLowerCase().includes('bug')) {
+        return `:bug2: ${linkedTask}`;
+    }
+    if (task.toLowerCase().includes('meeting')) {
+        return `:zoom: ${linkedTask}`;
+    }
+
+    return `:keyboard: ${linkedTask}`;
 }
